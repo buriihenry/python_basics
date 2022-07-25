b950d22c3ea70d2dd6b8888ce8a889d0d66d4d8d
buriihenry <buriihenry@gmail.com>
2022-07-25 20:19:05 +0300

    Updated If file

--- if.py
+++ if.py
@@ -1,5 +1,10 @@
-a = 33
-b = 200
-
-if b > a:
-  print("b is greater than a")
+x = int(input("Enter an interger:"))
+if x < 0:
+  x=0
+  print('Negative')
+elif x==0:
+  print('Zero')
+elif x==1:
+  print('Single')
+else:
+  print('More')     

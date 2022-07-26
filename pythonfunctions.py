45f822402cc8b34c19a808715b2ecce87254b7cb
buriihenry <buriihenry@gmail.com>
2022-07-26 07:49:59 +0300

    Python Function

--- pythonfunctions.py
+++ pythonfunctions.py
@@ -1,9 +1,7 @@
-#python functions
-
-#def my_function():
- # print("Hello World")
-#my_function()
-def my_function(fname, lname):
-	print(fname + " "+ lname)
-
-my_function("Henry", "Burii") 
+def evenOdd(x):
+	if(x % 2==0):
+		print("Even")
+	else:
+		print("Odd")
+evenOdd(2)
+evenOdd(7)			

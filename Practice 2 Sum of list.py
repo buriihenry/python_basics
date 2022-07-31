4fd18f4d1d42389b0417f1316ee8a743c84d1bd9
buriihenry <buriihenry@gmail.com>
2022-07-31 11:53:31 +0300

    Added code blocks

--- Practice 2 Sum of list.py
+++ Practice 2 Sum of list.py
@@ -1,6 +1,15 @@
+#solution for sum
 def sum (numbers):
     total =0
     for x in numbers:
         total +=x
     return total
 print(sum((8,2,3,0,7)))
+
+#solution for multiplication
+def multiply (numbers):
+    total =1
+    for x in numbers:
+        total *=x
+    return total
+print(multiply((8,2,3,1,7)))

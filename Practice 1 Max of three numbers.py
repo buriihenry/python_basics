24611b76b028194ef84c8aeef08b341aa90eb157
buriihenry <buriihenry@gmail.com>
2022-07-31 10:46:03 +0300

    Added code blocks

--- Practice 1 Max of three numbers.py
+++ Practice 1 Max of three numbers.py
@@ -14,3 +14,18 @@ x= 10
 y= 14
 z= 12
 print(maximum(x, y, z))
+
+#Solution 3
+
+i= 50
+j= 60
+k =70
+def maximum(i, j, k):
+    if(i>j) and (i>k):
+        largest=i
+    elif (j>i) and (j>k):
+        largest=j
+    else:
+        largest=k
+    return largest
+print(maximum (i,j,k))

86a033b91bb19b8603c67ace5833a278f75f6b84
buriihenry <buriihenry@gmail.com>
2022-08-03 06:09:41 +0300

    Added code blocks

---
+++ Class2.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, color, mileage):
+        self.color=color
+        self.mileage=mileage
+blue_car = Car(color="blue", mileage=20000)
+red_car = Car(color="red",mileage=30000)
+

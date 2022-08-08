3b693260235998bf3a5e853b5ed6bcbe7ea8a7cf
buriihenry <buriihenry@gmail.com>
2022-08-08 19:31:42 +0300

    Added code blocks

---
+++ Class3.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage=mileage
+ferrari = Vehicle(260,10000)
+print(ferrari.max_speed)
+print(ferrari.mileage)       

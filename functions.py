b63f906887f2a6aa225efbbf537542182be637fd
buriihenry <buriihenry@gmail.com>
2022-08-11 05:23:35 +0300

    Added code

--- functions.py
+++ functions.py
@@ -19,3 +19,15 @@ def even_number(n):
 		print('Odd Number')
 even_number(19)		
 
+
+print ('\n')
+# Version 2
+
+def is_even(list1):
+	even_num=[]
+	for n in list1:
+		if n % 2==0:
+			even_num.append(n)
+	return even_num
+even_num = is_even([2,3,10,15,20,24,9,62])
+print(even_num)		

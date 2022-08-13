f704b9ba1c790b067dd65994a3b859cd2fe4d8bd
buriihenry <buriihenry@gmail.com>
2022-08-12 23:15:10 -0700

    Added code

--- functions.py
+++ functions.py
@@ -30,4 +30,21 @@ def is_even(list1):
 			even_num.append(n)
 	return even_num
 even_num = is_even([2,3,10,15,20,24,9,62])
-print(even_num)		
+print(even_num)	
+
+print("\n")
+# Version 3
+def arithmetic(num1, num2):
+	add = num1 + num2
+	sub = num1 + num2
+	multiply = num1 * num2
+	division = num1 / num2
+
+	return add, sub, multiply, division
+
+a, b, c, d = arithmetic(10,2)
+
+print("Add:",a)
+print("sub:",b)
+print("multi:",c)
+print("division:",d)
